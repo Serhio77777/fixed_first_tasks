4bfbf6e9facd56773eddd27c3de29763c9da048c
Andrey Petrash <andrey@ekreative.com>
2016-07-14 13:01:08 +0300

    Changed animation; I hope I have fixed all

--- script.js
+++ script.js
@@ -11,41 +11,50 @@ var userName = document.getElementById("userName"),
 function validate() {
     var nameValueLength = userName.value.length,
         passwordLength = userPassword.value.length,
+        userPasswordCorrect=userPassword.value,
         userPasswordCorrectOnceMore = userPasswordInspection.value;
 	var regexpForValueNameAndPassword = /\W/g, regexpForPhone = /[^0-9]/g;
-    var inValidate = true;
+    var isInvalid = true;
 
-    function mistake(sendFieldName, sendFieldErrorName, top) {
+    function mistake(sendFieldName, sendFieldErrorName, left) {
         var wrong = true;
         sendFieldErrorName.style.visibility = "visible";
         sendFieldName.style.border = "2px solid red";
-        sendFieldErrorName.style.top = top + "%";
+        sendFieldErrorName.style.left = left + "%";
         wrong = false;
         return wrong;
     }
-    function correct(sendFieldName, sendFieldErrorName) {
+    function correct(sendFieldName, sendFieldErrorName, left) {
         sendFieldErrorName.style.visibility = "hidden";
         sendFieldName.style.border = "1px solid black";
+        sendFieldErrorName.style.left = left + "%";
     }
 
     if ((!userName.value) || (userName.value.search(regexpForValueNameAndPassword) != -1) || (nameValueLength<5)) {
-        inValidate = mistake(userName, errorName, 12.6);
-        // isInvalid
-
-	} else 	correct(userName, errorName);
-    if((!userPhone.value)||(userPhone.value.search(regexpForPhone) != -1)||(userPhone.value.length != 10)){
-          inValidate = mistake(userPhone, errorPhone, 25);
-    } else correct(userPhone, errorPhone);
-    if((!userPassword.value)||(userPassword.value.search(regexpForValueNameAndPassword) != -1)||(passwordLength<5)){
-          inValidate = mistake(userPassword, errorPassword, 41.2);
-    } else {
-       correct(userPassword, errorPassword);
+        isInvalid = mistake(userName, errorName, 45);
+	} else {
+        correct(userName, errorName, 10);
     }
-    if((passwordLength !== userPasswordCorrectOnceMore)){
-          inValidate = mistake(userPasswordInspection, errorPasswordInspection, 54);
-    } else correct(userPasswordInspection, errorPasswordInspection);
     
-    if (inValidate) {
+    if ((!userPhone.value)||(userPhone.value.search(regexpForPhone) != -1)||(userPhone.value.length != 10)){
+        isInvalid = mistake(userPhone, errorPhone, 45);
+    } else {
+        correct(userPhone, errorPhone, 10);
+    } 
+    
+    if ((!userPassword.value) || (userPassword.value.search(regexpForValueNameAndPassword) != -1) || (passwordLength<5)) {
+        isInvalid = mistake(userPassword, errorPassword, 45);
+    } else {
+        correct(userPassword, errorPassword, 10);
+    }
+    
+    if ((userPasswordCorrect !== userPasswordCorrectOnceMore)) {
+        isInvalid = mistake(userPasswordInspection, errorPasswordInspection, 45);
+    } else {
+        correct(userPasswordInspection, errorPasswordInspection, 10);
+    }
+    
+    if (isInvalid) {
         alert("Success! Takes my congratulations!");
     }
 	return false;
